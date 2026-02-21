adb06ff62b32bb5962185c9305969fd6d5c75989
KLINS-254 <klinswoo@gmail.com>
2026-02-21 21:24:41 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,88 @@
+//-------------------[ BOT SETTINGS ]------------------// 
+
+// @project_name : KLINS-MD 
+// @author : Ticoh 911 
+// @telegram : http://t.me/official_kango
+// @github : KLINS-254
+// @whatsapp : +254762573604
+
+//----------------------[ KANGO-XMD ]----------------------//
+
+const fs = require('fs')
+const { color } = require('./KLINSMD/color')
+if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
+
+
+//--------------------[ SESSION ID ]----------------------//
+
+global.SESSION_ID = process.env.SESSION_ID || '' 
+//Enter your KLINS-MD session id here; must start with KLINS-MD:~
+
+//--------------------[ BOT NAME ]----------------------//
+
+global.botname = process.env.BOT_NAME || 'KLINS-MD 
+
+//-----------------[ OWNER NUMBER ]------------------//
+
+global.ownernumber = process.env.OWNER_NUMBER || '254762573604' 
+
+//--------------------[ SUDO ]--------------------------//
+
+global.sudo = process.env.SUDO ? process.env.SUDO.split(',') : ['254762573604', '254765944780'];
+// Type additional allowed users here
+//NB: They'll be able to use every functions of the bot without restrictions.
+
+//-----------------[ OWNER NAME ]------------------//
+
+global.ownername = process.env.OWNER_NAME || 'Ticoh 911' 
+
+//------------[ STICKER PACKNAME ]-----------------//
+
+global.packname = process.env.STICKER_PACK_NAME || "KLINS-MD" 
+
+//--------------[ COUNTRY TIMEZONE ]------------//
+
+
+global.timezones = 'Africa/Kenya';  // Set this to you timezone
+
+
+
+//--------------[ STICKER AUTHOR NAME ]------------//
+
+global.author = process.env.STICKER_AUTHOR_NAME || "Ticoh" 
+
+//----------------[ GITHUB DATABASE ]-----------------//
+
+global.dbToken = process.env.GITHUB_TOKEN || "";
+
+
+//-----------------[ CONTEXT LINK ]--------------------//
+
+global.plink = process.env.PLINK || "https://youtube.com/@ticoh911"
+
+//------------------[ WATERMARK ]--------------------//
+
+global.wm = process.env.GL_WM || "> ©KLINS-MD"
+
+//---------------------[ REPLIES ]-----------------------//
+
+global.mess = { 
+  done: '*Done*', 
+  success: '©KLINS-MD, 
+  owner: `*You don't have permission to use this command!*`, 
+  group: '*This feature becomes available when you use it in a group!*', 
+  admin: '*You’ll unlock this feature with me as an admin!*', 
+  notadmin: '*This feature will work once you become an admin. A way of ensuring order!*' 
+}
+
+//--------------------[ WATCHER ]-----------------------//
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(color(`Updated '${__filename}'`, 'red'))
+  delete require.cache[file]
+  require(file)
+})
+
+//----------------------[ KANGO-XMD ]----------------------//
